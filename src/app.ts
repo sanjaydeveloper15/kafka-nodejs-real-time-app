a1b7fc905707360c4e28c58c4cc1e6855969fd0a
Sanjay Kumar <sanjaykumarwebs@gmail.com>
2025-01-12 19:01:06 +0530

    types added to server file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express from 'express'
+import express, { Application } from 'express'
 import { configDotenv } from 'dotenv'
 configDotenv()
-const app = express()
-const port = process.env.PORT
+const app: Application = express()
+const port: number = parseInt(process.env.PORT || '1000', 10) // 10 radix: decimal, 16 radix: hexadecimal, 8 radix: octal, securing 01000
 
 // middleware to parse json, available in express since 4.16 version, earlier same we were acheiving with body-parser package
 app.use(express.json({ limit: '5mb' }))
@@ -12,3 +12,5 @@ app.use(express.urlencoded({ limit: '5mb', extended: true }))
 app.listen(port, async () => {
     return console.log(`Server listening on port ${port}`)
 })
+
+export default app
